1dd7743f1c6bc802802aa26a32843c46e5f24f11
Guilherme Baldoino <guilherme.baldoino@softrh.com.br>
2019-12-13 12:00:30 -0200

    adicionando estrelinha

--- hohoho.py
+++ hohoho.py
@@ -1,26 +1,36 @@
+"""Hohoho."""
 from time import sleep
 from os import system
 
 E = '\033[0m'
 R = '\033[0;31m'
 G = '\033[0;32m'
+Y = '\033[1;33m'
 
 a = 1
 print ''
 for j in range(100):
     system('clear')
-    for i in range(10):
-        if ((i+j)%2==0):
-            print R + ' '*(10-(a//2))+'*'*a+' '*(10-(a//2)) + E
+    print '  '
+    print Y + ' ' * 11 + '    *' + E
+    print Y + ' ' * 11 + '   ***' + E
+    print Y + ' ' * 11 + ' *******' + E
+    print Y + ' ' * 11 + '  ** **' + E
+    print Y + ' ' * 11 + '  *   *' + E
+    for i in range(15):
+        if ((i + j) % 2 == 0):
+            print R + ' ' * (15 - (a // 2)) + '*' * a + ' ' * (
+                15 - (a // 2)) + E
             a += 2
         else:
-            print G + ' '*(10-(a//2))+'*'*a+' '*(10-(a//2)) + E
+            print G + ' ' * (15 - (a // 2)) + '*' * a + ' ' * (
+                15 - (a // 2)) + E
             a += 2
-    if(j%2==0):
-        print R + ' '*(a//2) + '*' + E
-        print G + ' '*(a//2) + '*' + E
+    if(j % 2 == 0):
+        print R + ' ' * (a // 2) + '*' + E
+        print G + ' ' * (a // 2) + '*' + E
     else:
-        print G + ' '*(a//2) + '*' + E
-        print R + ' '*(a//2) + '*' + E
+        print G + ' ' * (a // 2) + '*' + E
+        print R + ' ' * (a // 2) + '*' + E
     a = 1
     sleep(0.5)
